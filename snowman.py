c2a4e2faaf8bda1603e0fdd5dfc5db9438cd9f15
Samuel-Kugler <samuel-kugler@gmx.de>
2025-09-17 06:48:39 +0200

    added functions to run the game logic (display_game_state, get_game_guess_state) and improved main

--- snowman.py
+++ snowman.py
@@ -43,7 +43,6 @@ def user_input(guessed_letters: str) -> str:
     """
     Gets one letter from the user and checks that it wasn't guessed before.
     :param guessed_letters: str
-    :return: str
     """
     while True:
         guess = input("Guess a letter: ").strip().lower()
@@ -51,6 +50,7 @@ def user_input(guessed_letters: str) -> str:
         if not guess.isalpha() or not len(guess) == 1:
             print(f"{guess} is not a valid input! "
                   f"A valid input must be 1 letter only!")
+            continue
 
         if guess in guessed_letters:
             print(f"{guess} was already tried! Try a different letter!")
@@ -59,18 +59,48 @@ def user_input(guessed_letters: str) -> str:
         return guess
 
 
-def right_guess(guess: str, guessed_letters: str) -> bool:
+def right_guess(guess: str, secret_word: str) -> bool:
     """
     Checks if the user guessed right.
     :param guess: str
-    :param guessed_letters: str
+    :param secret_word:
     :return: bool
     """
-    return guess in guessed_letters
+    return guess in secret_word
+
+
+def get_game_guess_state(secret_word: str, guessed_letters: str) -> str:
+    """
+    Returns the current stage of the word.
+    :param secret_word: str
+    :param guessed_letters: str
+    :return: str
+    """
+    result = ""
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            result += letter
+        else:
+            result += "_"
+
+    return result
 
 
 def display_game_state(mistakes: int, secret_word: str, guessed_letters: str):
+    """
+    Displays the complete game state.
+    :param mistakes: int
+    :param secret_word: str
+    :param guessed_letters: str
+    """
+    current_word = get_game_guess_state(secret_word, guessed_letters)
+
+    #printing snowman
     print(STAGES[mistakes])
+
+    #printing guessing progress
+    print(" " * 5 + current_word + "\n")
 
 
 def play_game():
@@ -80,7 +110,7 @@ def play_game():
 
     # TODO: Build your game loop here.
     mistakes = 0
-    max_mistakes = 4
+    max_mistakes = len(STAGES) - 1
     guessed_letters = ""
 
     #starting snowman
@@ -92,10 +122,16 @@ def play_game():
         guessed_letters = guessed_letters + guess
 
         #display current snowman and guess
-        if not right_guess(guess, guessed_letters):
+        if not right_guess(guess, secret_word):
             mistakes += 1
 
         display_game_state(mistakes, secret_word, guessed_letters)
+
+    #user finish
+    if mistakes == max_mistakes:
+        print("The poor snowman melted...")
+    else:
+        print("Congratz! You saved the snowman!")
 
 
 if __name__ == "__main__":
